ec441c963eff152e411eab12ba4ebaa20b9ddc38
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-07-11 10:08:23 +0100

    Add all the files

--- queries.sql
+++ queries.sql
@@ -1,3 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name like '%mon';
+
+SElECT * from animals WHERE date_of_birth between '2016-01-01' and '2019-12-31';
+
+SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animals WHERE name IN ('Pikachu', 'Agumon');
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.50;
+
+SELECT * FROM animals WHERE neutered = TRUE;
+
+SELECT * FROM animals WHERE name NOT IN ('Gabumon');
+
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.40 and 17.30;
